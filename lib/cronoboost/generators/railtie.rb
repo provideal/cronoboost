dc60957717d5f1868bf327470abe4ba088622ec6
Philipp Hirsch <itself@hanspolo.net>
2018-04-24 16:17:09 +0200

    Add files for Third Party Tools and initializers

--- lib/cronoboost/generators/railtie.rb
+++ lib/cronoboost/generators/railtie.rb
@@ -2,6 +2,7 @@ module ActiveModel
   class Railtie < Rails::Railtie
     initializer 'generators' do |app|
       require 'cronoboost/generators/cronoboost/install_generator'
+      Rails::Generators.configure!(app.config.generators)
     end
   end
 end
