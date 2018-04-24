014530790f115c27575d54dc2cc85f8a216047d6
Philipp Hirsch <itself@hanspolo.net>
2018-04-24 16:48:28 +0200

    Set default filename to Cronoboost

--- lib/cronoboost/setup.rb
+++ lib/cronoboost/setup.rb
@@ -20,7 +20,8 @@ module Cronoboost
     ##
     #
     #
-    def self.run(path_to_init_file = '')
+    def self.run(path_to_init_file = nil)
+      path_to_init_file ||= (defined?(Rails) ? Rails.root : '') + 'Cronofile'
       (defined?(logger) ? logger : Logger.new(STDOUT)).info "Loading initializer file from '#{path_to_init_file}'"
       tasks = []
       File.open(path_to_init_file, 'r') do |f|
